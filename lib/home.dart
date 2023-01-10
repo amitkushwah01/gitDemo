e41c3cd61006036bc236fb033f9582974c506920
amit kushwah <amitmca000@gmail.com>
2023-01-10 23:13:19 +0530

    add new file home.dart

---
+++ lib/home.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class slas extends StatefulWidget
+{
+  @override
+  State<StatefulWidget> createState() {
+    return slasstate();
+  }
+}
+class slasstate extends State<slas>
+{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Container(color:Colors.green)
+    );
+  }
+}
